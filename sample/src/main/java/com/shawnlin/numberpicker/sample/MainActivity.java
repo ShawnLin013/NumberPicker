28d5a9195b4ad0e3c0e0cf60b45dcbde6ee5336b
stas <stanyslav.kozyrev@agilie.com>
2018-11-06 00:25:05 +0200

    added androidX support, sdk version updated

--- sample/src/main/java/com/shawnlin/numberpicker/sample/MainActivity.java
+++ sample/src/main/java/com/shawnlin/numberpicker/sample/MainActivity.java
@@ -2,9 +2,9 @@ package com.shawnlin.numberpicker.sample;
 
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import androidx.core.content.ContextCompat;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 
